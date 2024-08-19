a8a7f68f6d2b764ac3c1eb0500c030f59b2fcc15
Angelina Bushueva <113555244+AngelinaBush@users.noreply.github.com>
2024-08-19 17:13:04 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,17 +7,15 @@ const cors = require('cors')
 const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.set('strictQuery', false)
-//соединяем mongoose с базой данных
 
-const PORT = 7000 || process.env.port   //может быть соединен с другим портом
-
+const PORT = 7000 || process.env.port 
 app.use(express.json())
 app.use(cors())
 
 mongoose  
 .connect(process.env.MONGODB_LINK)
 .then(() => console.log('WE WERE CONNECTED TO MONGO'))
-.catch((err) => console.log(err))   //соединяем с базой данных 
+.catch((err) => console.log(err))  
 
 app.use(routes)
 
