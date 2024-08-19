a4feb9be4708ca6bf471ef5d22500d40e86f7eee
Angelina Bushueva <113555244+AngelinaBush@users.noreply.github.com>
2024-08-19 17:12:16 +0000

    Update MealModel.js

--- MealModel.js
+++ MealModel.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 const mealSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true // встроенный валидатор для подтверждения текстового ввода
+        required: true =
     }
 })
 
 module.exports = mongoose.model('Meal', mealSchema)
-
-//второй аргумент 'mealSchema' тот же, что и выше описанный "const mealSchema...."
